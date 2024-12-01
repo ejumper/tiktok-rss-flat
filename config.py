72652b49c6a39a0a0df353258d04906ebc564d32
ejumper <ericjumper@pm.me>
2024-12-01 11:03:42 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Set your GH Pages URL here e.g.:
 ghPagesURL = "https://ejumper.github.io/tiktok-rss-flat/"
-ghRawURL = "https://raw.githubusercontent.com/conoro/tiktok-rss-flat/main/"
+ghRawURL = "https://raw.githubusercontent.com/ejumper/tiktok-rss-flat/main/"
 
 # ghPagesURL = "https://tiktokrss.conoroneill.com/"
